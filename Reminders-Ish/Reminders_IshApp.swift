c629063057e5dce0ad1f7c6f49e142668604a6a6
Derrick White <gr8white.contact@gmail.com>
2024-12-30 10:21:23 -0600

    Initial Commit

---
+++ Reminders-Ish/Reminders_IshApp.swift
@@ -0,0 +1,17 @@
+//
+//  Reminders_IshApp.swift
+//  Reminders-Ish
+//
+//  Created by Cool-Ish on 12/30/24.
+//
+
+import SwiftUI
+
+@main
+struct Reminders_IshApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
